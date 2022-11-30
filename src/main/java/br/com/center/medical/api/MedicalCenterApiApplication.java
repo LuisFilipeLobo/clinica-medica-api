fe88df24bdbb45ce29cfa180dc7b387d5b466a70
Luis Filipe Lobo <luisfilipelobo.dev@gmail.com>
2022-11-30 12:32:56 -0300

    Base do projeto

---
+++ src/main/java/br/com/center/medical/api/MedicalCenterApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.center.medical.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCenterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalCenterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/center/medical/api/MedicalCenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.center.medical.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCenterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
