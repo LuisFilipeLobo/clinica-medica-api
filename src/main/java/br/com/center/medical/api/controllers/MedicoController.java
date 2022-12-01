d9ca281dac9c670f60e7f6305da204bfda06d1b3
Luis Filipe Lobo <luisfilipelobo.dev@gmail.com>
2022-11-30 21:33:30 -0300

    Corrigindo url da rota

--- src/main/java/br/com/center/medical/api/controllers/MedicoController.java
+++ src/main/java/br/com/center/medical/api/controllers/MedicoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/medicos")
+@RequestMapping("/api/v1/medicos")
 public class MedicoController {
 
     private MedicoService medicoService;
